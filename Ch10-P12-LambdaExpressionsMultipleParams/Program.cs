5b1ee608958f83977d77bb00a9f458648d196307
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-05 08:11:35 +0500

    final update

--- Ch10-P12-LambdaExpressionsMultipleParams/Program.cs
+++ Ch10-P12-LambdaExpressionsMultipleParams/Program.cs
@@ -10,6 +10,28 @@ namespace Ch10_P12_LambdaExpressionsMultipleParams
     {
         static void Main(string[] args)
         {
+            // Register with delegate as a lambda expression.
+            SimpleMath m = new SimpleMath();
+
+            //m.SetMathHandler((msg, result) =>
+            //{
+            //    Console.WriteLine("Message: {0}, Result: {1}", msg, result);
+            //});
+
+            //m.SetMathHandler((string msg,int result) =>
+            //{
+            //    Console.WriteLine("Message: {0}, Result: {1}", msg, result);
+            //});
+
+            m.SetMathHandler(() =>
+            {
+                Console.WriteLine("Message: {0}, Result: {1}", msg, result);
+            });
+
+            // This will execute the lambda expression.
+            m.Add(10, 10);
+
+            Console.ReadLine();
         }
     }
 }
